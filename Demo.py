967093d9c49f0bebcee93377239eeee11367c70d
Abdullah Hasan Shah <hasan8293574@gmail.com>
2025-06-27 09:51:03 +0500

    Update Demo.py

--- Demo.py
+++ Demo.py
@@ -1,6 +1,2 @@
-#compute gross pay bye hours and rate per hour
-hrs=input("Enter Hour:")
-rate=input("Eenter Rate per Hour:")
-pay=float(hrs)*float(rate)
-
-print("Pay:", pay)
+#this is my first python program
+print("Hello World")
